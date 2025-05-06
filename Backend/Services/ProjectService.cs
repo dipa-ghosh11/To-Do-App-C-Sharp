9cd6206ccab7a51c8c7954bcc02a223a37c45246
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-06 15:09:14 +0530

    All request added in project service

--- Backend/Services/ProjectService.cs
+++ Backend/Services/ProjectService.cs
@@ -1,8 +1,30 @@
 using System;
+using Backend.Models;
+using MongoDB.Driver;
 
 namespace Backend.Services;
 
 public class ProjectService
 {
+    private readonly IMongoCollection<Project> _projects;
 
+    public ProjectService(DatabaseSettings settings)
+    {
+        var client = new MongoClient(settings.ConnectionString);
+        var database = client.GetDatabase(settings.DatabaseName);
+        _projects = database.GetCollection<Project>(settings.ProjectCollectionName);
+    }
+
+
+    public async Task<List<Project>> GetAll() => await _projects.Find(Project => true).ToListAsync();
+
+    public async Task<Project> GetById(string id) => await _projects.Find(Project => Project.Id == id).FirstOrDefaultAsync();
+
+    public async Task Add(Project Project) => await _projects.InsertOneAsync(Project);
+
+    public async Task Update(string id, Project updatedProject) =>
+        await _projects.ReplaceOneAsync(Project => Project.Id == id, updatedProject);
+
+    public async Task Delete(string id) =>
+        await _projects.DeleteOneAsync(Project => Project.Id == id);
 }
