d034de1a6d556c2f831df7c35ee585a1fc033a4f
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-06 15:17:24 +0530

    Project controller updated

--- Backend/Controllers/ProjectController.cs
+++ Backend/Controllers/ProjectController.cs
@@ -1,3 +1,6 @@
+using Backend.Models;
+using Backend.Services;
+using Backend.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,7 +8,127 @@ namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProjectController : ControllerBase
+    public class ProjectController(ProjectService projectService) : ControllerBase
     {
+        private readonly ProjectService _projectService=projectService;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Project>>> GetAll()
+        {
+            try
+            {
+                var projects = await _projectService.GetAll();
+                return Ok(new ApiResponse<List<Project>>
+                {
+                    Success = projects != null,
+                    Message = projects != null ? "projects fetched" : "projects not found",
+                    Data = projects
+                });
+            }
+            catch (System.Exception e)
+            {
+
+                return BadRequest(new ApiResponse<List<Project>>
+                {
+                    Success = false,
+                    Message = e.Message
+                });
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetById(string id)
+        {
+            try
+            {
+                var Project = await _projectService.GetById(id);
+                return Ok(new ApiResponse<Project>
+                {
+                    Success = Project != null,
+                    Message = Project != null ? "Project fetched" : "Project not found",
+                    Data = Project
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ApiResponse<Project>
+                {
+                    Success = false,
+                    Message = e.Message
+                });
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Project>> Add(Project Project)
+        {
+            try
+            {
+                await _projectService.Add(Project);
+                return Ok(new ApiResponse<Project>
+                {
+                    Success = true,
+                    Message = "Project added",
+                    Data = Project
+                });
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(new ApiResponse<Project>
+                {
+                    Success = false,
+                    Message = e.Message
+                });
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(string id, Project Project)
+        {
+            try
+            {
+                await _projectService.Update(id, Project);
+                return Ok(new ApiResponse<Project>
+                {
+                    Success = true,
+                    Message = "Project updated",
+                    Data = Project
+                });
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(new ApiResponse<Project>
+                {
+                    Success = false,
+                    Message = e.Message
+                });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                await _projectService.Delete(id);
+                return Ok(new ApiResponse<Project>
+                {
+                    Success = true,
+                    Message = "Project deleted"
+                });
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(new ApiResponse<Project>
+                {
+                    Success = false,
+                    Message = e.Message
+                });
+            }
+        }
+
     }
 }
